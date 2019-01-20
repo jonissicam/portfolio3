5ef081ffbcad285a9d615b143009f2f21401efb3
jonis <jonissicam@gmail.com>
2019-01-20 17:44:03 +0800

    portfolio v3.3

--- partials/navbar.php
+++ partials/navbar.php
@@ -2,7 +2,7 @@
 	<section class="row">
 		<div class="col-lg-12 p-0">
 			<nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
-				<a id="welcome" class="navbar-brand mr-0" href="./index.php">Hello, Welcome to my portfolio</a>
+				<a id="welcome" class="navbar-brand mr-0" href="index.php">Hello, Welcome to my portfolio</a>
 
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMenu">
 					<span class="navbar-toggler-icon"></span>
@@ -10,8 +10,8 @@
 
 				<div class="collapse navbar-collapse" id="navbarMenu">
 					<ul class="navbar-nav ml-auto">
-						<li class="nav-item"><a class="nav-link" href="./project.php">Projects</a></li>
-						<li class="nav-item"><a class="nav-link" href="./about.php">About</a></li>
+						<li class="nav-item"><a class="nav-link" href="project.php">Projects</a></li>
+						<li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
 						<li class="nav-item"><p class="nav-link" onclick="openNav()">Contact</p></li>
 					</ul>
 				</div>
