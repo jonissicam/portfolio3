1940c8596573d66c8a7f9aba024b1df95d90e829
jonis <jonissicam@gmail.com>
2019-01-20 09:58:57 +0800

    portfolio v3

--- partials/start_body.php
+++ partials/start_body.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="x-ua-compatible" content="ie-edge">
+	<link rel="icon" type="image/png" href="../images/icon.png">
 	<!-- fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
